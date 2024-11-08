43701da7dc6d642223aa56f39a6aa920dd157b01
Dubovskiyproger <dubr0vskiyA@yandex.ru>
2024-11-08 18:01:24 +0300

    Make draft of main page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,92 @@
 @extends('layouts.main')
 
 @section('content')
-    <div>Index of albums</div>
+    <div class="container mx-auto px-4 pt-16">
+        <div class="popular-music">
+            <h2 class="uppercase tracking-wide text-green-500 text-lg font-semibold">Popular Music</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-16">
+                <div class="mt-8">
+                    <a href="#">
+                        <img src='/img/timeless.jpg' alt="Timeless" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">Timeless</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span>The Weeknd</span>
+                            <span class="mx-2">|</span>
+                            <span>2024</span>
+                        </div>
+                        <div class="text-gray-400">
+                            R&B, Hip-Hop
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src='/img/timeless.jpg' alt="Timeless" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">Timeless</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span>The Weeknd</span>
+                            <span class="mx-2">|</span>
+                            <span>2024</span>
+                        </div>
+                        <div class="text-gray-400">
+                            R&B, Hip-Hop
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src='/img/timeless.jpg' alt="Timeless" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">Timeless</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span>The Weeknd</span>
+                            <span class="mx-2">|</span>
+                            <span>2024</span>
+                        </div>
+                        <div class="text-gray-400">
+                            R&B, Hip-Hop
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src='/img/timeless.jpg' alt="Timeless" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">Timeless</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span>The Weeknd</span>
+                            <span class="mx-2">|</span>
+                            <span>2024</span>
+                        </div>
+                        <div class="text-gray-400">
+                            R&B, Hip-Hop
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src='/img/timeless.jpg' alt="Timeless" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">Timeless</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span>The Weeknd</span>
+                            <span class="mx-2">|</span>
+                            <span>2024</span>
+                        </div>
+                        <div class="text-gray-400">
+                            R&B, Hip-Hop
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,22 +12,22 @@
 
 <body class="font-sans bg-gray-900 text-white">
     <nav class="border-b border-gray-800">
-        <div class="container-mx-auto flex items-center justify-between px-4 py-6">
-            <ul class="flex items-center">
+        <div class="container mx-auto flex flex-col md:flex-row items-center justify-between px-4 py-6">
+            <ul class="flex flex-col md:flex-row items-center">
                 <li><a href="#">
                         <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
                         <svg width="35px" height="35px" viewBox="0 0 1024 1024" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg">
                             <path d="M918.3 74.7c-13.3-11.2-30.8-15.9-47.9-12.8l-503.3 88.8c-28.1 5-48.5 29.3-48.5 57.8v500.8c-24.9-21.2-57.1-34.1-92.3-34.1-78.6 0-142.6 64-142.6 142.6s64 142.6 142.6 142.6 142.6-64 142.6-142.6V369.2L889 277.4v331.2c-24.9-21.2-57.1-34.1-92.3-34.1-78.6 0-142.6 64-142.6 142.6s64 142.6 142.6 142.6 142.6-64 142.6-142.6V119.7c0-17.4-7.6-33.8-21-45zM226.3 910c-50.9 0-92.3-41.4-92.3-92.3s41.4-92.3 92.3-92.3 92.3 41.4 92.3 92.3-41.4 92.3-92.3 92.3z m142.6-591.9V208.5c0-4.1 2.9-7.6 6.9-8.3l503.3-88.8c3.4-0.5 5.7 0.9 6.8 1.9 1.1 0.9 3 3 3 6.4v106.7l-520 91.7z m427.8 491.3c-50.9 0-92.3-41.4-92.3-92.3s41.4-92.3 92.3-92.3 92.3 41.4 92.3 92.3-41.4 92.3-92.3 92.3z" fill="#3CB87F" />
                         </svg>
                     </a></li>
-                <li class="ml-16">
+                <li class="md:ml-16">
                     <a href="#" class="hover:text-gray-300">Albums</a>
                 </li>
-                <li class="ml-6">
+                <li class="md:ml-6">
                     <a href="#" class="hover:text-gray-300">Artists</a>
                 </li>
             </ul>
-            <div class="flex items-center">
+            <div class="flex flex-col md:flex-row items-center">
                 <div class="relative">
                     <input type="text" class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1
                     focus:outline-none focus:shadow-outline" placeholder="Search">
@@ -38,6 +38,14 @@
                         </svg>
 
                     </div>
+                </div>
+                <div class="ml-4">
+                <a href="#">
+                <svg width="35" height="35" viewBox="0 0 25 24" fill="#ffffff" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M16.4337 6.35C16.4337 8.74 14.4937 10.69 12.0937 10.69L12.0837 10.68C9.69365 10.68 7.74365 8.73 7.74365 6.34C7.74365 3.95 9.70365 2 12.0937 2C14.4837 2 16.4337 3.96 16.4337 6.35ZM14.9337 6.34C14.9337 4.78 13.6637 3.5 12.0937 3.5C10.5337 3.5 9.25365 4.78 9.25365 6.34C9.25365 7.9 10.5337 9.18 12.0937 9.18C13.6537 9.18 14.9337 7.9 14.9337 6.34Z" fill="#ffffff"/>
+<path d="M12.0235 12.1895C14.6935 12.1895 16.7835 12.9395 18.2335 14.4195V14.4095C20.2801 16.4956 20.2739 19.2563 20.2735 19.4344L20.2735 19.4395C20.2635 19.8495 19.9335 20.1795 19.5235 20.1795H19.5135C19.0935 20.1695 18.7735 19.8295 18.7735 19.4195C18.7735 19.3695 18.7735 17.0895 17.1535 15.4495C15.9935 14.2795 14.2635 13.6795 12.0235 13.6795C9.78346 13.6795 8.05346 14.2795 6.89346 15.4495C5.27346 17.0995 5.27346 19.3995 5.27346 19.4195C5.27346 19.8295 4.94346 20.1795 4.53346 20.1795C4.17346 20.1995 3.77346 19.8595 3.77346 19.4495L3.77345 19.4448C3.77305 19.2771 3.76646 16.506 5.81346 14.4195C7.26346 12.9395 9.35346 12.1895 12.0235 12.1895Z" fill="#ffffff"/>
+</svg>
+                </a>
                 </div>
             </div>
         </div>
