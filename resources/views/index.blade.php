185f39cfcc17b1eff5d38fda358b905b05c0ff25
Dubovskiyproger <dubr0vskiyA@yandex.ru>
2024-12-20 17:49:41 +0300

    Adding example code for 'similar albums' section (albums and button 'more')

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,14 +7,14 @@
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-16">
                 <div class="mt-8">
                     <a href="#">
-                        <img src='/img/dark-side.jpg' alt="Timeless" class="hover:opacity-75 transition ease-in-out duration-150">
+                        <img src='/img/dark-side.jpg' alt="The Dark Side of the Moon" class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
                     <div class="mt-2">
                         <a href="#" class="text-lg mt-2 hover:text-gray-300">The Dark Side of the Moon</a>
                         <div class="flex items-center text-gray-400 text-sm mt-1">
                             <span>Pink Floyd</span>
                             <span class="mx-2">|</span>
-                            <span>2024</span>
+                            <span>1973</span>
                         </div>
                         <div class="text-gray-400">
                         Rock
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -181,7 +181,90 @@
 
     <div class="similar-albums">
         <div class="container mx-auto px-4 py-16">
-            <h3 class="text-4xl font-semibold">Similar Albums</h3>
+            <h2 class="text-4xl font-semibold">Similar Albums</h2>
+            <div class="grid grid-cols-4 sm:grid-cols-2 md:grid-cols-4 gap-8">
+                <div class="mt-8">
+                    <a href="#">
+                        <img src='/img/power-and-the-passion.jpg' alt="Power And The Passion" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">Power And The Passion</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span>Eloy</span>
+                            <span class="mx-2">|</span>
+                            <span>1975</span>
+                        </div>
+                        <div class="text-gray-400">
+                        Rock
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src='/img/the-show-goose.jpg' alt="The Show Goose" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">The Show Goose</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span>Camel</span>
+                            <span class="mx-2">|</span>
+                            <span>1975</span>
+                        </div>
+                        <div class="text-gray-400">
+                        Rock
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src='/img/power-and-the-passion.jpg' alt="Power And The Passion" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">Power And The Passion</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span>Eloy</span>
+                            <span class="mx-2">|</span>
+                            <span>1975</span>
+                        </div>
+                        <div class="text-gray-400">
+                        Rock
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src='/img/the-show-goose.jpg' alt="The Show Goose" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">The Show Goose</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span>Camel</span>
+                            <span class="mx-2">|</span>
+                            <span>1975</span>
+                        </div>
+                        <div class="text-gray-400">
+                        Rock
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="mt-8 flex items-center space-x-2">
+                <span class="text-lg">More</span>
+                <svg fill="#fff" height="18px" width="18px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 330 330" xml:space="preserve" stroke="#000000">
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        <path id="XMLID_225_" d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393 c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393 s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"></path>
+                    </g>
+                </svg>
+            </button>
+            
+        </div>
+    </div><!-- end similar albums -->
+
+    <div class="comments">
+        <div class="container mx-auto px-4 py-16">
+            
         </div>
     </div>
 @endsection
